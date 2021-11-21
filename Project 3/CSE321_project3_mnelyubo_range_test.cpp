de348c9705e9dc69d48a4adfc81974242ff5733e
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-11-20 23:05:01 -0500

    eliminated wire crossing, added stabilizer array

--- Project 3/CSE321_project3_mnelyubo_range_test.cpp
+++ Project 3/CSE321_project3_mnelyubo_range_test.cpp
@@ -15,8 +15,8 @@
 *
 *   Constraints:    Range Detection Sensor must be connected to the Nucleo with the following pins:
 *                     Vcc  - 5V
-*                     Trig - PC_8
-*                     Echo - PC_9
+*                     Trig - PC_9
+*                     Echo - PC_8
 *                     Gnd  - GND
 *
 *   References:
@@ -30,50 +30,59 @@
 #include <chrono>
 
 
-// Blinking rate in milliseconds
+//timing behavior
 #define POLLING_HIGH_TIME     10us
-#define POLLING_CYCLE_TIME_MS 500
+#define POLLING_CYCLE_TIME_MS 100
+
+//data type alias
 #define ull unsigned long long
+
+//output stabilization buffer data
+#define stabilizerArrayLen 40
+#define DISTANCE_MAXIMUM 400  /* sensor max range is stated to be 4m */
 
 void riseHandler();
 void fallHandler();
 
-Timer timer;
-
 ull getTimeSinceStart();
+int getStableDistance();
+
+int stableDistance[stabilizerArrayLen];
+int stableDistIdx = 0;
+
+Timer timer;
 
 ull riseDetected = 0;
 ull fallDetected = 0;
 
 // DigitalOut trig(PC_8);
-InterruptIn echo(PC_9);
+InterruptIn echo(PC_8);
 
 int main(){
 
+    //create rise and fall timers for input port
     echo.rise(riseHandler);
     echo.fall(fallHandler);
 
-    RCC->AHB2ENR |= 0x4;    //enable clock to port C
+    //enable port C
+    RCC->AHB2ENR |= 0x4;
 
-    //configure pin C8 as an output
-    GPIOC->MODER &= ~(0x20000);
-    GPIOC->MODER |= 0x10000;
+    //configure pin C9 as an output
+    GPIOC->MODER &= ~(0x80000);
+    GPIOC->MODER |= 0x40000;
 
-    //configure pin C9 as an input
-    GPIOC->MODER &= ~(0xC0000);
-
-    //printf("\nInitial Input State: %d\n", GPIOC->IDR & 0x200);
+    //configure pin C8 as an input
+    GPIOC->MODER &= ~(0x30000);
 
     while(true){
         timer.start();
-        //printf("=== beginning signal broadcast at %llu ===\n", getTimeSinceStart());
-        GPIOC->ODR |= 0x100;
-        wait_us(10);
-        GPIOC->ODR &= ~(0x100);
-        ull endOfHigh = getTimeSinceStart();
 
-        thread_sleep_for(60);   //documentation states sleep for 60ms before measuring
-        //printf("=== ended signal broadcast at %llu\n", endOfHigh);
+        //send trigger signal high for 10 us
+        GPIOC->ODR |= 0x200;
+        wait_us(10);
+        GPIOC->ODR &= ~(0x200);
+
+        thread_sleep_for(60);   //sensor documentation states wait for 60ms before measuring upper limit
 
 
         while(getTimeSinceStart() < POLLING_CYCLE_TIME_MS * 1000){
@@ -81,7 +90,11 @@ int main(){
                 ull deltaTime = fallDetected - riseDetected;
                 //printf("Rise and Fall Detected! Rise Time: %llu\tFall Time: %llu\tDelta Time: %llu\n", riseDetected, fallDetected, deltaTime);
                 int distance = deltaTime / 58;
-                printf("Distance estimate: %d cm\n", distance);   //documentation states divide by 58 to provide distance in CM
+                if(distance < DISTANCE_MAXIMUM){
+                    stableDistance[stableDistIdx++ % stabilizerArrayLen]=distance;
+                }
+
+                printf("Sample measured: %d cm \tStabilized estimate: %d\n", distance, getStableDistance());   //documentation states divide by 58 to provide distance in CM
                 riseDetected=false;
                 fallDetected=false;
             }
@@ -107,6 +120,14 @@ void riseHandler(){
     riseDetected=getTimeSinceStart();
 }
 
+
+int getStableDistance(){
+    int sum = 0;
+    for(int i = 0; i < stabilizerArrayLen; i++){
+        sum += stableDistance[i];
+    }
+    return sum / stabilizerArrayLen;
+}
 
 //Access global object timer to get the time since the process began in microseconds
 //code in this function is from https://os.mbed.com/docs/mbed-os/v6.15/apis/timer.html
