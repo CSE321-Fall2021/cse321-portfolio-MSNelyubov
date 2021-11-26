47e0f2793d12c2a0800b9f8687251b3591efaf10
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-11-26 12:45:06 -0500

    disable testing files to prevent confusion of main

--- Project 3/CSE321_project3_mnelyubo_range_test.cpp
+++ Project 3/CSE321_project3_mnelyubo_range_test.cpp
@@ -1,135 +1,135 @@
-/******************************************************************************
-*   File Name:      CSE321_project3_mnelyubo_range_test.cpp
-*   Author:         Misha Nelyubov (mnelyubo@buffalo.edu)
-*   Date Created:   11/20/2021
-*   Last Modified:  11/20/2021
-*   Purpose:        This program tests the operation of the range detection sensor
-*
-*   Functions:      
-*
-*   Assignment:     Project 3
-*
-*   Inputs:         Range Detection Sensor
-*
-*   Outputs:        Serial printout
-*
-*   Constraints:    Range Detection Sensor must be connected to the Nucleo with the following pins:
-*                     Vcc  - 5V
-*                     Trig - PC_9
-*                     Echo - PC_8
-*                     Gnd  - GND
-*
-*   References:
-*       HC-SR04 audio sensor datasheet:    https://www.digikey.com/htmldatasheets/production/1979760/0/0/1/hc-sr04.html
-*       NUCLEO datasheet:                  https://www.st.com/resource/en/reference_manual/dm00310109-stm32l4-series-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
-*       MBED OS API: timer                 https://os.mbed.com/docs/mbed-os/v6.15/apis/timer.html
-*
-******************************************************************************/
+// /******************************************************************************
+// *   File Name:      CSE321_project3_mnelyubo_range_test.cpp
+// *   Author:         Misha Nelyubov (mnelyubo@buffalo.edu)
+// *   Date Created:   11/20/2021
+// *   Last Modified:  11/20/2021
+// *   Purpose:        This program tests the operation of the range detection sensor
+// *
+// *   Functions:      
+// *
+// *   Assignment:     Project 3
+// *
+// *   Inputs:         Range Detection Sensor
+// *
+// *   Outputs:        Serial printout
+// *
+// *   Constraints:    Range Detection Sensor must be connected to the Nucleo with the following pins:
+// *                     Vcc  - 5V
+// *                     Trig - PC_9
+// *                     Echo - PC_8
+// *                     Gnd  - GND
+// *
+// *   References:
+// *       HC-SR04 audio sensor datasheet:    https://www.digikey.com/htmldatasheets/production/1979760/0/0/1/hc-sr04.html
+// *       NUCLEO datasheet:                  https://www.st.com/resource/en/reference_manual/dm00310109-stm32l4-series-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
+// *       MBED OS API: timer                 https://os.mbed.com/docs/mbed-os/v6.15/apis/timer.html
+// *
+// ******************************************************************************/
 
-#include "mbed.h"
-#include <chrono>
-
-
-//timing behavior
-#define POLLING_HIGH_TIME     10us
-#define POLLING_CYCLE_TIME_MS 100
-
-//data type alias
-#define ull unsigned long long
-
-//output stabilization buffer data
-#define stabilizerArrayLen 40
-#define DISTANCE_MAXIMUM 400  /* sensor max range is stated to be 4m */
-
-void riseHandler();
-void fallHandler();
-
-ull getTimeSinceStart();
-int getStableDistance();
-
-int stableDistance[stabilizerArrayLen];
-int stableDistIdx = 0;
-
-Timer timer;
-
-ull riseDetected = 0;
-ull fallDetected = 0;
-
-// DigitalOut trig(PC_8);
-InterruptIn echo(PC_8);
-
-int main(){
-
-    //create rise and fall timers for input port
-    echo.rise(riseHandler);
-    echo.fall(fallHandler);
-
-    //enable port C
-    RCC->AHB2ENR |= 0x4;
-
-    //configure pin C9 as an output
-    GPIOC->MODER &= ~(0x80000);
-    GPIOC->MODER |= 0x40000;
-
-    //configure pin C8 as an input
-    GPIOC->MODER &= ~(0x30000);
-
-    while(true){
-        timer.start();
-
-        //send trigger signal high for 10 us
-        GPIOC->ODR |= 0x200;
-        wait_us(10);
-        GPIOC->ODR &= ~(0x200);
-
-        thread_sleep_for(60);   //sensor documentation states wait for 60ms before measuring upper limit
+// #include "mbed.h"
+// #include <chrono>
 
 
-        while(getTimeSinceStart() < POLLING_CYCLE_TIME_MS * 1000){
-            if(riseDetected && fallDetected){
-                ull deltaTime = fallDetected - riseDetected;
-                //printf("Rise and Fall Detected! Rise Time: %llu\tFall Time: %llu\tDelta Time: %llu\n", riseDetected, fallDetected, deltaTime);
-                int distance = deltaTime / 58;
-                if(distance < DISTANCE_MAXIMUM){
-                    stableDistance[stableDistIdx++ % stabilizerArrayLen]=distance;
-                }
+// //timing behavior
+// #define POLLING_HIGH_TIME     10us
+// #define POLLING_CYCLE_TIME_MS 100
 
-                printf("Sample measured: %d cm \tStabilized estimate: %d\n", distance, getStableDistance());   //documentation states divide by 58 to provide distance in CM
-                riseDetected=false;
-                fallDetected=false;
-            }
-        }
+// //data type alias
+// #define ull unsigned long long
+
+// //output stabilization buffer data
+// #define stabilizerArrayLen 40
+// #define DISTANCE_MAXIMUM 400  /* sensor max range is stated to be 4m */
+
+// void riseHandler();
+// void fallHandler();
+
+// ull getTimeSinceStart();
+// int getStableDistance();
+
+// int stableDistance[stabilizerArrayLen];
+// int stableDistIdx = 0;
+
+// Timer timer;
+
+// ull riseDetected = 0;
+// ull fallDetected = 0;
+
+// // DigitalOut trig(PC_8);
+// InterruptIn echo(PC_8);
+
+// int main(){
+
+//     //create rise and fall timers for input port
+//     echo.rise(riseHandler);
+//     echo.fall(fallHandler);
+
+//     //enable port C
+//     RCC->AHB2ENR |= 0x4;
+
+//     //configure pin C9 as an output
+//     GPIOC->MODER &= ~(0x80000);
+//     GPIOC->MODER |= 0x40000;
+
+//     //configure pin C8 as an input
+//     GPIOC->MODER &= ~(0x30000);
+
+//     while(true){
+//         timer.start();
+
+//         //send trigger signal high for 10 us
+//         GPIOC->ODR |= 0x200;
+//         wait_us(10);
+//         GPIOC->ODR &= ~(0x200);
+
+//         thread_sleep_for(60);   //sensor documentation states wait for 60ms before measuring upper limit
+
+
+//         while(getTimeSinceStart() < POLLING_CYCLE_TIME_MS * 1000){
+//             if(riseDetected && fallDetected){
+//                 ull deltaTime = fallDetected - riseDetected;
+//                 //printf("Rise and Fall Detected! Rise Time: %llu\tFall Time: %llu\tDelta Time: %llu\n", riseDetected, fallDetected, deltaTime);
+//                 int distance = deltaTime / 58;
+//                 if(distance < DISTANCE_MAXIMUM){
+//                     stableDistance[stableDistIdx++ % stabilizerArrayLen]=distance;
+//                 }
+
+//                 printf("Sample measured: %d cm \tStabilized estimate: %d\n", distance, getStableDistance());   //documentation states divide by 58 to provide distance in CM
+//                 riseDetected=false;
+//                 fallDetected=false;
+//             }
+//         }
         
-        //prevent half-returns from cascading into the next data point
-        riseDetected=false;
-        fallDetected=false;
+//         //prevent half-returns from cascading into the next data point
+//         riseDetected=false;
+//         fallDetected=false;
 
-        timer.stop();
-        timer.reset();
-    }
-    return 0;
-}
+//         timer.stop();
+//         timer.reset();
+//     }
+//     return 0;
+// }
 
 
-void fallHandler(){
-    fallDetected=getTimeSinceStart();
-}
+// void fallHandler(){
+//     fallDetected=getTimeSinceStart();
+// }
 
-void riseHandler(){
-    riseDetected=getTimeSinceStart();
-}
+// void riseHandler(){
+//     riseDetected=getTimeSinceStart();
+// }
 
-int getStableDistance(){
-    int sum = 0;
-    for(int i = 0; i < stabilizerArrayLen; i++){
-        sum += stableDistance[i];
-    }
-    return sum / stabilizerArrayLen;
-}
+// int getStableDistance(){
+//     int sum = 0;
+//     for(int i = 0; i < stabilizerArrayLen; i++){
+//         sum += stableDistance[i];
+//     }
+//     return sum / stabilizerArrayLen;
+// }
 
-//Access global object timer to get the time since the process began in microseconds
-//code in this function is from https://os.mbed.com/docs/mbed-os/v6.15/apis/timer.html
-ull getTimeSinceStart() {
-    using namespace std::chrono;
-    return duration_cast<microseconds>(timer.elapsed_time()).count();
-}
+// //Access global object timer to get the time since the process began in microseconds
+// //code in this function is from https://os.mbed.com/docs/mbed-os/v6.15/apis/timer.html
+// ull getTimeSinceStart() {
+//     using namespace std::chrono;
+//     return duration_cast<microseconds>(timer.elapsed_time()).count();
+// }
--- Project 3/CSE321_project3_mnelyubo_thread_test.cpp
+++ Project 3/CSE321_project3_mnelyubo_thread_test.cpp
@@ -1,83 +1,83 @@
-/******************************************************************************
-*   File Name:      CSE321_project3_mnelyubo_range_test.cpp
-*   Author:         Misha Nelyubov (mnelyubo@buffalo.edu)
-*   Date Created:   11/22/2021
-*   Last Modified:  11/22/2021
-*   Purpose:        This test code verifies the expected behavior of threads, 
-*                     event queues, and mutexes.  These scheduling utilities
-*                     will be used in the main project implementation.
-*
-*   Functions:      N/A
-*
-*   Assignment:     Project 3
-*
-*   Inputs:         None
-*
-*   Outputs:        Serial printout
-*
-*   Constraints:    N/A
-*
-*   References:
-*       NUCLEO datasheet:                  https://www.st.com/resource/en/reference_manual/dm00310109-stm32l4-series-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
-*       MBED OS API: Mutex                 https://os.mbed.com/docs/mbed-os/v6.15/apis/mutex.html
-*
-******************************************************************************/
+// /******************************************************************************
+// *   File Name:      CSE321_project3_mnelyubo_range_test.cpp
+// *   Author:         Misha Nelyubov (mnelyubo@buffalo.edu)
+// *   Date Created:   11/22/2021
+// *   Last Modified:  11/22/2021
+// *   Purpose:        This test code verifies the expected behavior of threads, 
+// *                     event queues, and mutexes.  These scheduling utilities
+// *                     will be used in the main project implementation.
+// *
+// *   Functions:      N/A
+// *
+// *   Assignment:     Project 3
+// *
+// *   Inputs:         None
+// *
+// *   Outputs:        Serial printout
+// *
+// *   Constraints:    N/A
+// *
+// *   References:
+// *       NUCLEO datasheet:                  https://www.st.com/resource/en/reference_manual/dm00310109-stm32l4-series-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
+// *       MBED OS API: Mutex                 https://os.mbed.com/docs/mbed-os/v6.15/apis/mutex.html
+// *
+// ******************************************************************************/
 
-#include "mbed.h"
-#include <chrono>
+// #include "mbed.h"
+// #include <chrono>
 
-Thread t1;
-Thread t2;
-Thread t3;
+// Thread t1;
+// Thread t2;
+// Thread t3;
 
-Mutex printer;  //create mutex for controlling access to serial output printing
+// Mutex printer;  //create mutex for controlling access to serial output printing
 
-//the amount of times that each output function has been called
-int beat[3] = {0,0,0};
+// //the amount of times that each output function has been called
+// int beat[3] = {0,0,0};
 
-Ticker tick1;
-Ticker tick2;
-Ticker tick3;
+// Ticker tick1;
+// Ticker tick2;
+// Ticker tick3;
 
-void outputInit1();
-void outputInit2();
-void outputInit3();
-void output(int beatThread);
+// void outputInit1();
+// void outputInit2();
+// void outputInit3();
+// void output(int beatThread);
 
-EventQueue eq1(32 * EVENTS_EVENT_SIZE);
-EventQueue eq2(32 * EVENTS_EVENT_SIZE);
-EventQueue eq3(32 * EVENTS_EVENT_SIZE);
+// EventQueue eq1(32 * EVENTS_EVENT_SIZE);
+// EventQueue eq2(32 * EVENTS_EVENT_SIZE);
+// EventQueue eq3(32 * EVENTS_EVENT_SIZE);
 
-int main(){
-    printf("== Beginning execution ==\n");
-    t1.start(callback(&eq1, &EventQueue::dispatch_forever));    //start thread 1 to process event queue 1
-    t2.start(callback(&eq2, &EventQueue::dispatch_forever));    //start thread 2 to process event queue 2
-    t3.start(callback(&eq3, &EventQueue::dispatch_forever));    //start thread 3 to process event queue 3
+// int main(){
+//     printf("== Beginning execution ==\n");
+//     t1.start(callback(&eq1, &EventQueue::dispatch_forever));    //start thread 1 to process event queue 1
+//     t2.start(callback(&eq2, &EventQueue::dispatch_forever));    //start thread 2 to process event queue 2
+//     t3.start(callback(&eq3, &EventQueue::dispatch_forever));    //start thread 3 to process event queue 3
 
-    tick1.attach(&outputInit1, 1s);       //set ticker 1 to run the output initializer once every second for thread 1
-    tick2.attach(&outputInit2, 1s);       //set ticker 2 to run the output initializer once every second for thread 2
-    tick3.attach(&outputInit3, 1s);       //set ticker 3 to run the output initializer once every second for thread 3
+//     tick1.attach(&outputInit1, 1s);       //set ticker 1 to run the output initializer once every second for thread 1
+//     tick2.attach(&outputInit2, 1s);       //set ticker 2 to run the output initializer once every second for thread 2
+//     tick3.attach(&outputInit3, 1s);       //set ticker 3 to run the output initializer once every second for thread 3
 
-    while(true){}                       //run indefinitely
+//     while(true){}                       //run indefinitely
 
-    return 0;
-}
+//     return 0;
+// }
 
-//these ISR functions adds the time-costly thread functions to the event queue
-void outputInit1(){eq1.call(output, 1);}
-void outputInit2(){eq2.call(output, 2);}
-void outputInit3(){eq3.call(output, 3);}
+// //these ISR functions adds the time-costly thread functions to the event queue
+// void outputInit1(){eq1.call(output, 1);}
+// void outputInit2(){eq2.call(output, 2);}
+// void outputInit3(){eq3.call(output, 3);}
 
-//this EventQueue-called function executes lengthier calls
-void output(int beatThread){
-    printer.lock();
-    printf("thread %d rise %d\n", beatThread, ++beat[beatThread]);
-    printer.unlock();
+// //this EventQueue-called function executes lengthier calls
+// void output(int beatThread){
+//     printer.lock();
+//     printf("thread %d rise %d\n", beatThread, ++beat[beatThread]);
+//     printer.unlock();
     
-    thread_sleep_for(500);
+//     thread_sleep_for(500);
 
-    printer.lock();
-    printf("thread %d fall %d\n", beatThread, beat[beatThread]);
-    printer.unlock();
-}
+//     printer.lock();
+//     printf("thread %d fall %d\n", beatThread, beat[beatThread]);
+//     printer.unlock();
+// }
 
