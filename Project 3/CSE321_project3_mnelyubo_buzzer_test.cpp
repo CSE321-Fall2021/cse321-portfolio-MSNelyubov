19b988f47138092cefb474ce16ffc3f6d6f02470
argothenaut <54564357+MSNelyubov@users.noreply.github.com>
2021-12-01 18:11:44 -0500

    initialized buzzer frequency test file

---
+++ Project 3/CSE321_project3_mnelyubo_buzzer_test.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+*   File Name:      CSE321_project3_mnelyubo_buzzer_test.cpp
+*   Author:         Misha Nelyubov (mnelyubo@buffalo.edu)
+*   Date Created:   12/01/2021
+*   Last Modified:  12/01/2021
+*   Purpose:        This file tests the effect of various frequency inputs 
+*                     on the buzzer output.
+*
+*   Functions:      
+*
+*   Assignment:     Project 3
+*
+*   Inputs:         None
+*
+*   Outputs:        Buzzer
+*
+*   Constraints:    Buzzer must be connected to the system with the following pins:
+*                       TODO
+*
+*   References:     TODO
+*
+******************************************************************************/
+
+#include <mbed.h>
+
+
+int main() {
+
+  while(1) {
+
+  }
+
+  return 0;
+}
